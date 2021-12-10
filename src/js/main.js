532de5f8288941afce615c6267f10c3660f9eb53
Laura Carbajales <laura.carbajales@gmail.com>
2021-12-10 12:28:46 +0100

    input js

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,10 @@
 // }
 // collapsableHeader.addEventListener('click', handlerClickHeader);
 
+//<----------------- Colapsable (versión 2) ----------------->
+
 const collapsableHeader = document.querySelector('.js_collapsable_title');
-function handlerClickHeader() {
+function handlerClickHeader(event) {
   event.currentTarget.parentNode.classList.toggle('collapsed');
 }
 collapsableHeader.addEventListener('click', handlerClickHeader);
@@ -17,3 +19,23 @@ collapsableHeader2.addEventListener('click', handlerClickHeader);
 
 const collapsableHeader3 = document.querySelector('.js_collapsable2_title');
 collapsableHeader3.addEventListener('click', handlerClickHeader);
+
+//<----------------- Input ----------------->
+
+const nameInput = document.querySelector('.js_name');
+const jobInput = document.querySelector('.js_job');
+const emailInput = document.querySelector('.js_email');
+
+const namePreview = document.querySelector('.js_preview_name');
+const namePreview = document.querySelector('.js_preview_email');
+
+function handleKeyName() {
+  console.log(nameInput.value);
+  namePreview.innerHTML = nameInput.value;
+}
+function handleKeyEmail() {
+  console.log(emailInput.value);
+  emailPreview.href = `mailito: ${emailInput.value}`;
+}
+nameInput.addEventListener('keyup', handleKeyName);
+emailInput.addEventListener('keyup', handleKeyEmail);
