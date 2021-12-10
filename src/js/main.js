da85324d4cdaada640b1c810bb324e3fb0f4f115
Paula <zocchiedro@gmail.com>
2021-12-10 13:19:23 +0100

    make element collapse

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
+//traer HTML
+
+const collapsableDesignElement = document.querySelector ('.js-collapsable-title');
+const collapsableDesignContent = document.querySelector('.js-design-content');
+const collapsableArrow = document.querySelector('.js-arrow');
+
+//crear handler
+function handleClickHeader () {
+    if (collapsableDesignContent.classList.contains('collapsed')) {
+        collapsableDesignContent.classList.toggle('collapsed');
+        collapsableArrow.classList.toggle('rotate');
+    }
+    else {
+        collapsableDesignContent.classList.toggle('collapsed');
+        collapsableArrow.classList.toggle('rotate');
+    }
+}
+
+//crear listener
+
+collapsableDesignElement.addEventListener('click', handleClickHeader);
 
