d1d6f04d515c74bae31721c0fb91352fc266f2b4
Laura Carbajales <laura.carbajales@gmail.com>
2021-12-10 12:17:41 +0100

    collapsed

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
+// const collapsableHeader = document.querySelector ('.js_collapsable_title');
 
+// function handlerClickHeader() {
+//     collapsableHeader.classList.add('collapsed');
+// }
+// collapsableHeader.addEventListener('click', handlerClickHeader);
 
+const collapsableHeader = document.querySelector('.js_collapsable_title');
+function handlerClickHeader() {
+  event.currentTarget.parentNode.classList.toggle('collapsed');
+}
+collapsableHeader.addEventListener('click', handlerClickHeader);
+
+const collapsableHeader2 = document.querySelector('.js_collapsable2_title');
+collapsableHeader2.addEventListener('click', handlerClickHeader);
+
+const collapsableHeader3 = document.querySelector('.js_collapsable2_title');
+collapsableHeader3.addEventListener('click', handlerClickHeader);
