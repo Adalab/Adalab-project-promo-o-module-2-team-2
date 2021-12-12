8c3a122bae0045adcce8de43bc457b582129e2f6
Laura Carbajales <laura.carbajales@gmail.com>
2021-12-12 20:48:46 +0100

    update coments

--- src/js/main.js
+++ src/js/main.js
@@ -46,26 +46,27 @@ const collapsableTitle3 = document.querySelector('.js_collapsable_title_share');
 
 //Hadler para todos
 function handlerClickTitle(event) {
+  //Para escribir menos, meto "event.currentTarget" en una constante
   const elementSelected = event.currentTarget;
-
+  //Condicional para que se colapse el elemento que queda abierto (y su flecha gire)
   if (!event.currentTarget.parentNode.classList.contains('collapsed')) {
-    event.currentTarget.parentNode.classList.toggle('collapsed');
+    elementSelected.parentNode.classList.toggle('collapsed');
     elementSelected.querySelector('.js-arrow').classList.toggle('rotate');
     return;
   }
   //Elemento HTML comun para todos los title
   const collapsableTitleList = document.querySelectorAll('.js_title_list');
 
-  //Bucle para que por defecto se cierre el resto de secciones al abrir una
+  //Bucle para que los elementos de "collapsableTitleList" sean recorridos uno a uno y colapsados (e igual con la flecha).
   for (let i = 0; i < collapsableTitleList.length; i++) {
     collapsableTitleList[i].parentNode.classList.add('collapsed');
     collapsableTitleList[i].querySelector('.js-arrow').classList.remove('rotate');
   }
-  //funcion hadler
-
+  //Colapsar y rotar flecha al clickar
   elementSelected.parentNode.classList.toggle('collapsed');
   elementSelected.querySelector('.js-arrow').classList.toggle('rotate');
 }
+
 //Listener para design
 collapsableTitle1.addEventListener('click', handlerClickTitle);
 //Listener para fill
